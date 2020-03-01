d2931a6a75f93dba517f02bce4d6d3a07a2427ef
Aditya Chouksey <aditya.chouksey734@gmail.com>
2020-03-01 16:53:25 +0530

    first commit

---
+++ Controllers/CyclesController.cs
@@ -0,0 +1,23 @@
+﻿using StoreMVC.Models;
+using System.Web.Mvc;
+
+namespace StoreMVC.Controllers
+{
+	public class CyclesController : Controller
+	{
+		// GET: Cycles/Random
+		public ActionResult Random()
+		{
+			var cycle = new Cycle() { Name = "Mountain Bike" };
+			//return View(cycle);
+			return RedirectToAction("Index", "Home");
+		}
+		//^:Regex string opening, $: Regex string closing
+		[Route("Cycles/Release/{Year:regex(^\\d{4}$)}/{Month:regex(^\\d{2}$):range(1, 12)}")]
+		public ActionResult ByReleaseDate(int year, int month)
+		{
+			return Content($"{year}/{month}");			
+		}
+
+	}
+}
---
+++ Models/Cycle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreMVC.Models
+{
+	public class Cycle
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
